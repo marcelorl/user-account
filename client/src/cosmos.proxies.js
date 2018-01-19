6fc98dd9420bd41e0bab7f34babf1be59ddc4485
marcelorl <marcelo_rl2@hotmail.com>
2018-01-19 02:45:48 -0200

    tests: cosmos configuration

---
+++ client/src/cosmos.proxies.js
@@ -0,0 +1,16 @@
+import createReduxProxy from 'react-cosmos-redux-proxy';
+import createRouterProxy from 'react-cosmos-router-proxy';
+// We can import app files here
+import configureStore from './reducers';
+
+// Read more about configuring Redux in the Redux proxy section below
+const ReduxProxy = createReduxProxy({
+  createStore: state => configureStore(state)
+});
+
+// We ensure a specific proxy order
+export default [
+  // Not all proxies have options, and often relying on defaults is good enough
+  createRouterProxy(),
+  ReduxProxy
+];
